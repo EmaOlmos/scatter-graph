bdeb7f5e97fdbf9913fe9e5c1c616d0b94a3817c
EmaOlmos <eduardoolmosdionisio@gmai.com>
2024-04-06 20:38:56 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import * as d3 from "d3";
+
+function App() {
+  useEffect(() => {}, []);
+  return (
+    <>
+      <h1>AAA</h1>
+    </>
+  );
+}
+
+export default App;
