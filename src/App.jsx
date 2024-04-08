615006e513b7529e3498be3b1a831201cf6ddad5
EmaOlmos <eduardoolmosdionisio@gmai.com>
2024-04-07 18:10:56 -0600

    can see the data representated by circles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 
 function App() {
@@ -12,23 +12,20 @@ function App() {
       const margin = 50;
       const width = 800;
       const height = 600; // svg scale
+      let time = [];
 
+      d.forEach((d) => {
+        const splitTime = d.Time.split(":");
+        time.push(new Date(1970, 0, 1, 0, splitTime[0], splitTime[1]));
+      });
       const years = d.map((e) => e.Year); // map years and time to use in axis
-      const time = d.map((e) => e.Time);
+      //const time = d.map((e) => e.Time);
 
       const yearsMin = d3.min(years); // min and max of both variables
       const yearsMax = d3.max(years);
 
-      const timeMin = d3.min(
-        time.map((e) => {
-          return e.replace(":", ".");
-        })
-      );
-      const timeMax = d3.max(
-        time.map((e) => {
-          return e.replace(":", ".");
-        })
-      );
+      const timeMin = d3.min(time);
+      const timeMax = d3.max(time);
 
       // svg config
       const svg = d3
@@ -38,18 +35,33 @@ function App() {
         .style("background-color", "aliceblue");
 
       // config scales to make the axises
-      const x = d3.scaleLinear([yearsMin, yearsMax], [margin, width - margin]);
-      const y = d3.scaleLinear([timeMin, timeMax], [margin, height - margin]);
+      const x = d3.scaleLinear(
+        [yearsMin - 1, yearsMax + 1],
+        [margin, width - margin]
+      );
+
+      const y = d3.scaleTime([timeMin, timeMax], [margin, height - margin]);
 
       const axisBottom = svg
         .append("g")
         .attr("transform", `translate(0,${height - margin})`)
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
       const axisLeft = svg
         .append("g")
         .attr("transform", `translate(${margin}, 0)`)
-        .call(d3.axisLeft(y));
+        .call(d3.axisLeft(y).tickFormat(d3.timeFormat("%M:%S")));
+
+      // console.log(d);
+
+      svg
+        .selectAll("circle")
+        .data(d)
+        .join("circle")
+        .attr("r", 5)
+        .attr("fill", "blue")
+        .attr("cx", (d, i) => x(d.Year))
+        .attr("cy", (d, i) => y(time[i]));
     });
   }, []);
   return (
